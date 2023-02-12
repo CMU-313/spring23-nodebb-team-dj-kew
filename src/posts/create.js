95909716e018a12a88d069afdb014ae7920b02be
Keren Huang <kerenhuang@kerens-mbp-2.wifi.local.cmu.edu>
2023-02-12 10:14:36 -0500

    Made new create.ts file and still passes lint/test

--- src/posts/create.js
+++ src/posts/create.js
@@ -1,16 +1,17 @@
-'use strict';
-
-const _ = require('lodash');
-
-const meta = require('../meta');
-const db = require('../database');
-const plugins = require('../plugins');
-const user = require('../user');
-const topics = require('../topics');
-const categories = require('../categories');
-const groups = require('../groups');
-const utils = require('../utils');
-
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const lodash_1 = __importDefault(require("lodash"));
+const meta_1 = __importDefault(require("../meta"));
+const database_1 = __importDefault(require("../database"));
+const plugins_1 = __importDefault(require("../plugins"));
+const user_1 = __importDefault(require("../user"));
+const topics_1 = __importDefault(require("../topics"));
+const categories_1 = __importDefault(require("../categories"));
+const groups_1 = __importDefault(require("../groups"));
+const utils_1 = __importDefault(require("../utils"));
 module.exports = function (Posts) {
     Posts.create = async function (data) {
         // This is an internal method, consider using Topics.reply instead
@@ -20,16 +21,13 @@ module.exports = function (Posts) {
         const content = data.content.toString();
         const timestamp = data.timestamp || Date.now();
         const isMain = data.isMain || false;
-
         if (!uid && parseInt(uid, 10) !== 0) {
             throw new Error('[[error:invalid-uid]]');
         }
-
-        if (data.toPid && !utils.isNumber(data.toPid)) {
+        if (data.toPid && !utils_1.default.isNumber(data.toPid)) {
             throw new Error('[[error:invalid-pid]]');
         }
-
-        const pid = await db.incrObjectField('global', 'nextPid');
+        const pid = await database_1.default.incrObjectField('global', 'nextPid');
         let postData = {
             pid: pid,
             uid: uid,
@@ -38,48 +36,42 @@ module.exports = function (Posts) {
             timestamp: timestamp,
             isAnon: isAnon,
         };
-
         if (data.toPid) {
             postData.toPid = data.toPid;
         }
-        if (data.ip && meta.config.trackIpPerPost) {
+        if (data.ip && meta_1.default.config.trackIpPerPost) {
             postData.ip = data.ip;
         }
         if (data.handle && !parseInt(uid, 10)) {
             postData.handle = data.handle;
         }
-
-        let result = await plugins.hooks.fire('filter:post.create', { post: postData, data: data });
+        let result = await plugins_1.default.hooks.fire('filter:post.create', { post: postData, data: data });
         postData = result.post;
-        await db.setObject(`post:${postData.pid}`, postData);
-
-        const topicData = await topics.getTopicFields(tid, ['cid', 'pinned']);
+        await database_1.default.setObject(`post:${postData.pid}`, postData);
+        const topicData = await topics_1.default.getTopicFields(tid, ['cid', 'pinned']);
         postData.cid = topicData.cid;
-
         await Promise.all([
-            db.sortedSetAdd('posts:pid', timestamp, postData.pid),
-            db.incrObjectField('global', 'postCount'),
-            user.onNewPostMade(postData),
-            topics.onNewPostMade(postData),
-            categories.onNewPostMade(topicData.cid, topicData.pinned, postData),
-            groups.onNewPostMade(postData),
+            database_1.default.sortedSetAdd('posts:pid', timestamp, postData.pid),
+            database_1.default.incrObjectField('global', 'postCount'),
+            user_1.default.onNewPostMade(postData),
+            topics_1.default.onNewPostMade(postData),
+            categories_1.default.onNewPostMade(topicData.cid, topicData.pinned, postData),
+            groups_1.default.onNewPostMade(postData),
             addReplyTo(postData, timestamp),
             Posts.uploads.sync(postData.pid),
         ]);
-
-        result = await plugins.hooks.fire('filter:post.get', { post: postData, uid: data.uid });
+        result = await plugins_1.default.hooks.fire('filter:post.get', { post: postData, uid: data.uid });
         result.post.isMain = isMain;
-        plugins.hooks.fire('action:post.save', { post: _.clone(result.post) });
+        plugins_1.default.hooks.fire('action:post.save', { post: lodash_1.default.clone(result.post) });
         return result.post;
     };
-
     async function addReplyTo(postData, timestamp) {
         if (!postData.toPid) {
             return;
         }
         await Promise.all([
-            db.sortedSetAdd(`pid:${postData.toPid}:replies`, timestamp, postData.pid),
-            db.incrObjectField(`post:${postData.toPid}`, 'replies'),
+            database_1.default.sortedSetAdd(`pid:${postData.toPid}:replies`, timestamp, postData.pid),
+            database_1.default.incrObjectField(`post:${postData.toPid}`, 'replies'),
         ]);
     }
 };
