b39c59c99ba7d8b56db2de53771be80a875bac25
Jacky-He <39445499+Jacky-He@users.noreply.github.com>
2023-03-19 16:53:34 -0400

    test if workflow succeeds if we don't throw error

--- test/dynamic_tests/category_data_test.js
+++ test/dynamic_tests/category_data_test.js
@@ -3,5 +3,5 @@ const file = require('../../src/file')
 
 module.exports = function run_test() {
     let stage = Iroh.Stage('file.walk("../dynamic_tests")');
-    assert(false);
+    // assert(false);
 }
--- test/dynamic_tests/index.js
+++ test/dynamic_tests/index.js
@@ -1,3 +1,4 @@
 // runs all IROH tests
 require('./iroh_example_test')()
 require('./category_data_test')()
+console.log("All iroh tests passed!")
